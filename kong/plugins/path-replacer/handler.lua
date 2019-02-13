196755a1b5adbf57abd8ae7522ff4b8d46fb78f6
Marton Papp <marton.papp@emarsys.com>
2019-02-13 14:19:22 +0100

    CDPIP-1077: Interpolate header content into path

--- kong/plugins/path-replacer/handler.lua
+++ kong/plugins/path-replacer/handler.lua
@@ -10,6 +10,14 @@ end
 
 function PathReplacerHandler:access(conf)
   PathReplacerHandler.super.access(self)
+
+  local replacement = kong.request.get_header("X-Suite-CustomerId")
+
+  if not replacement then return end
+
+  local upstream_uri = ngx.var.upstream_uri:gsub("~customer_id~", replacement)
+  kong.service.request.set_path(upstream_uri)
+
 end
 
 return PathReplacerHandler
--- spec/path-replacer/plugin_e2e_spec.lua
+++ spec/path-replacer/plugin_e2e_spec.lua
@@ -14,13 +14,13 @@ describe("PathReplacer", function()
     helpers.db:truncate()
   end)
 
-  it('should interpolate the X-Suite-CustomerId header into the {customer_id} placeholder', function()
+  it('should interpolate the X-Suite-CustomerId header into the ~customer_id~ placeholder', function()
     local service_creation_call = assert(helpers.admin_client():send({
       method = 'POST',
       path = '/services',
       body = {
         name = 'MockBin',
-        url = 'http://mockbin:8080/request'
+        url = 'http://mockbin:8080/request/~customer_id~'
       },
       headers = {
         ['Content-Type'] = 'application/json'
@@ -52,7 +52,7 @@ describe("PathReplacer", function()
       method = 'POST',
       path = '/services/' .. service_id .. '/plugins',
       body = {
-        name = 'path-replacer'
+        name = 'path-replacer',
       },
       headers = {
         ['Content-Type'] = 'application/json'
@@ -74,5 +74,64 @@ describe("PathReplacer", function()
     )
 
     assert.is_equal('http://0.0.0.0/request/112233/some-resource-path', client_response_data.url)
+  end)
+
+  it('should not interpolate when X-Suite-CustomerId header is not present', function()
+    local service_creation_call = assert(helpers.admin_client():send({
+      method = 'POST',
+      path = '/services',
+      body = {
+        name = 'MockBin',
+        url = 'http://mockbin:8080/request/~customer_id~'
+      },
+      headers = {
+        ['Content-Type'] = 'application/json'
+      }
+    }))
+
+    local service_creation_data = cjson.decode(
+      assert.res_status(201, service_creation_call)
+    )
+
+    local service_id = service_creation_data.id
+
+    local route_creation_call = assert(helpers.admin_client():send({
+      method = 'POST',
+      path = '/services/' .. service_id .. '/routes',
+      body = {
+        paths = {
+          '/test'
+        }
+      },
+      headers = {
+        ['Content-Type'] = 'application/json'
+      }
+    }))
+
+    assert.res_status(201, route_creation_call)
+
+    local plugin_creation_call = assert(helpers.admin_client():send({
+      method = 'POST',
+      path = '/services/' .. service_id .. '/plugins',
+      body = {
+        name = 'path-replacer',
+      },
+      headers = {
+        ['Content-Type'] = 'application/json'
+      }
+    }))
+
+    assert.res_status(201, plugin_creation_call)
+
+    local client_call = assert(helpers.proxy_client():send({
+      method = 'GET',
+      path = '/test/some-resource-path'
+    }))
+
+    local client_response_data = cjson.decode(
+      assert.res_status(200, client_call)
+    )
+
+    assert.is_equal('http://0.0.0.0/request/~customer_id~/some-resource-path', client_response_data.url)
   end)
 end)
